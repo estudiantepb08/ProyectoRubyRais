68819204f63f99b2fd349415e1171e7b416b006a
AlexH <estudiantepb08@gmail.com>
2018-09-05 21:33:24 -0500

    Clase 11 modelo vista controlador

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all
   end
+
+
+  def new
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def create
+
+    end
+    
+
 end
+
+
+
